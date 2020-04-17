7ef8aefa37deb7ad4603fd2d4e931f404ce4f789
Rostyslaw <rostykzadyraichuk010@gmail.com>
2020-04-17 20:22:39 +0300

    petryshak repository clone

---
+++ src/main/java/com/example/ConnectionManager.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import java.sql.Connection;
+
+public final class ConnectionManager {
+
+    private ConnectionManager(){}
+
+//    public static Connection createConnection() {
+//
+//    }
+}
--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,4 +1,32 @@
 package com.example;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
+
+    public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        String url = "jdbc:mysql://localhost:3306/shop";
+        String userName = "root";
+        String password = "";
+        System.out.println("Connection start");
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(url, userName, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
